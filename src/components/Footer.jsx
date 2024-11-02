5c4162f7c5f053de93ea93c9da70bba11532ed2a
hervekwizera <hervekwizera63@gmail.com>
2024-11-02 09:20:45 +0200

    i write some code in footer and nav bar

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+// src/components/Footer.js
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2024 My Portfolio | Connect with me on <a href="https://github.com/yourusername">GitHub</a></p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+// src/components/Navbar.js
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h2>My Portfolio</h2>
+      <div className="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/contact">Contact</Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
